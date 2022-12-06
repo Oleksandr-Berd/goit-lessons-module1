7d71c5618ec490e79225dc8c93ceffb29f46d52f
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-12-06 15:48:39 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Footer } from './Footer/Footer';
+import { Header } from './Header/Header';
+import { Hero } from './Hero/Hero';
+import { Posts } from './Posts/Posts';
+import './App.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      {/* <Loader /> */}
+      <Header />
+      <Hero />
+      <Posts />
+      <Footer />
     </div>
   );
 };
+
+// const Loader = ({ progress = 0.5 }) => {
+//   return (
+//     <div style={{ width: progress * 100 + '%' }} className="progress">
+//       Progress: {progress}
+//     </div>
+//   );
+// };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import { Container } from 'components/Container/Container';
+
+export const Footer = () => {
+  return (
+    <Container>
+      <footer>2012-2022</footer>
+    </Container>
+  );
+};
